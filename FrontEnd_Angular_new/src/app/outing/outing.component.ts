f49e63fd6c45912451d4739fa207ae78759df121
Nishant Chhattani <Nishant.Chhattani@gmail.com>
2021-04-26 22:04:26 +0530

    frontend_new

---
+++ FrontEnd_Angular_new/src/app/outing/outing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outing',
+  templateUrl: './outing.component.html',
+  styleUrls: ['./outing.component.css']
+})
+export class OutingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FrontEnd_Angular_new/src/app/tracinglist/tracinglist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracinglist',
+  templateUrl: './tracinglist.component.html',
+  styleUrls: ['./tracinglist.component.css']
+})
+export class TracinglistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
